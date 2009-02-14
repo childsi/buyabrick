3bf71ad0fb5e441ab209bdffce37c6fea9c30684
Patrick Sinclair <metade@livro.lan>
2009-02-14 15:18:45 +0000

    Removed all activemerchant order stuff

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "activemerchant", :lib => "active_merchant", :version => "1.4.1"
+
+  #config.gem "activemerchant", :lib => "active_merchant", :version => "1.4.1"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
