fbf299ed887714709f842ebb16c1ad353a135582
Patrick Sinclair <metade@livro.lan>
2009-03-08 16:55:26 +0000

    Made brick value generation more realistic

--- lib/forgery/forgeries/brick_forgery.rb
+++ lib/forgery/forgeries/brick_forgery.rb
@@ -1,4 +1,13 @@
 class BrickForgery < Forgery
+  def self.value
+    case (rand)
+      when 0..0.5 then MonetaryForgery.money(:min => 200, :max => 9_99)
+      when 0.5..0.7 then MonetaryForgery.money(:min => 10_00, :max => 19_99)
+      when 0.7..0.9 then MonetaryForgery.money(:min => 20_00, :max => 49_99)
+      else MonetaryForgery.money(:min => 49_99, :max => 499_99)
+    end
+  end
+  
   def self.brick_params
     first, last = NameForgery.first_name, NameForgery.last_name
     domain = InternetForgery.domain_name
@@ -8,7 +17,7 @@ class BrickForgery < Forgery
       :email => (rand > 0.5) ? "#{first}.#{last}@#{domain}" : nil,
       :url => (rand > 0.5) ? "http://#{domain}" : nil,
       :message => LoremIpsumForgery.sentence,
-      :value => MonetaryForgery.money(:min => 200, :max => 499_99),
+      :value => value,
       :show_value => (rand > 0.2),
       :purchased_at => (rand > 0.2) ? Time.now : nil,
     }
