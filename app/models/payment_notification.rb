987a2f7c6b519de53c8050f8460c2354a35faa63
Patrick Sinclair <metade@livro.lan>
2009-03-08 19:25:38 +0000

    Posting to twitter when a brick is purchased

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -4,7 +4,15 @@ class PaymentNotification < ActiveRecord::Base
   after_create :mark_brick_as_purchased
   
   private
+  
   def mark_brick_as_purchased
-    brick.update_attribute(:purchased_at, Time.now) if status == 'OK'
+    if status == 'OK'
+      brick.update_attribute(:purchased_at, Time.now)
+      FuKing::Twitter.update("#{brick.name} has just bought a brick: #{brick_url}")
+    end
+  end
+  
+  def brick_url
+    "http://buyabrick.heroku.com/bricks/#{brick.url_key}"
   end
 end
