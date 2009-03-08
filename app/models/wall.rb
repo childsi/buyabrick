99ba8f0e18311d70fc25d543b9248bafd40e8d56
Patrick Sinclair <metade@livro.lan>
2009-03-08 16:58:52 +0000

    Wall needs 160 bricks

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -2,7 +2,7 @@ class Wall
   attr_accessor :bricks, :total
   
   def initialize
-    self.bricks = Brick.find(:all, :conditions => 'purchased_at IS NOT NULL', :order => 'purchased_at DESC', :limit => 150)
+    self.bricks = Brick.find(:all, :conditions => 'purchased_at IS NOT NULL', :order => 'purchased_at DESC', :limit => 160)
     self.total = Brick.sum(:value, :conditions => 'purchased_at IS NOT NULL') / 100.0
   end
   
