8052c0d9f46eee2d55f7894723954efbccc3057d
Patrick Sinclair <metade@livro.lan>
2009-03-08 14:35:39 +0000

    Tidied up wall CSS

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -2,7 +2,15 @@ class Wall
   attr_accessor :bricks, :total
   
   def initialize
-    self.bricks = Brick.find(:all, :conditions => 'purchased_at IS NOT NULL', :order => 'purchased_at DESC', :limit => 100)
+    self.bricks = Brick.find(:all, :conditions => 'purchased_at IS NOT NULL', :order => 'purchased_at DESC', :limit => 150)
     self.total = Brick.sum(:value, :conditions => 'purchased_at IS NOT NULL') / 100.0
+  end
+  
+  def rows
+    @rows ||= bricks.in_groups_of(16).reverse
+  end
+  
+  def empty_rows
+    10 - rows.size
   end
 end
