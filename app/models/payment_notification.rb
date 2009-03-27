1179804437448a58b5d7c0c96d73a8a91f88c5a1
Patrick Sinclair <metade@livro.config>
2009-03-27 15:56:09 +0000

    Added logging on payment notification

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -7,8 +7,11 @@ class PaymentNotification < ActiveRecord::Base
   
   def mark_brick_as_purchased
     if status == 'OK'
+      logger.info("Brick #{brick.url_key} purchased successfully")
       brick.update_attribute(:purchased_at, Time.now)
       FuKing::Twitter.update("#{brick.name} has just bought a brick: #{brick_url}")
+    else
+      logger.info("Brick #{brick.url_key} purchase unsuccessful: #{status}")
     end
   end
   
