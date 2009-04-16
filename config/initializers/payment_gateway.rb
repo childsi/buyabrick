d987e924988d3a91a7564cffe7febd6a3c649634
Patrick Sinclair <metade@livro.lan>
2009-04-16 23:25:01 +0100

    Using new heroku configuration

--- config/initializers/payment_gateway.rb
+++ config/initializers/payment_gateway.rb
@@ -1,7 +1,13 @@
 config_path = File.join(RAILS_ROOT, 'config', 'payment_gateway.yml')
 if File.exists?(config_path)
-  gateway_options = YAML.load_file(config_path)
-  GATEWAY = ProtxForm.new(gateway_options[RAILS_ENV.to_sym])
+  gateway_options = YAML.load_file(config_path)[RAILS_ENV.to_sym]
 else
-  $stderr.puts "Payment gateway not set up"
+  gateway_options = {
+    :url => ENV['PAYMENT_GATEWAY_URL'],
+    :login: ENV['PAYMENT_GATEWAY_LOGIN'],
+    :protocol: ENV['PAYMENT_GATEWAY_PROTOCOL'],
+    :password: ENV['PAYMENT_GATEWAY_PASSWORD'],
+    :encryption_key: ENV['PAYMENT_GATEWAY_KEY'],
+  }
 end
+GATEWAY = ProtxForm.new(gateway_options)
