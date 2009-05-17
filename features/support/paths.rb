98f1819777e782178d6cbe39c4a94c1deae7aabb
Patrick Sinclair <metade@livro.lan>
2009-05-17 18:27:37 +0100

    Upgraded to latest version of cucumber

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,18 +1,33 @@
-def path_to(page_name)
-  case page_name
-  
-  when /the homepage/i
-    root_path
-  when /the bricks page/i
-    bricks_path
-  when /the naughty bricks page/i
-    naughties_bricks_path
-  when /the new (.+) brick page/i
-    brick_new_with_value_path($1)
-  
-  # Add more page name => path mappings here
-  
-  else
-    raise "Can't find mapping from \"#{page_name}\" to a path."
+module NavigationHelpers
+  # Maps a name to a path. Used by the
+  #
+  #   When /^I go to (.+)$/ do |page_name|
+  #
+  # step definition in webrat_steps.rb
+  #
+  def path_to(page_name)
+    case page_name
+    
+    when /the home page/i
+      root_path
+    when /the bricks page/i
+      bricks_path
+    when /the naughty bricks page/i
+      naughties_bricks_path
+    when /the new (.+) brick page/i
+      brick_new_with_value_path($1)
+    
+    # Add more mappings here.
+    # Here is a more fancy example:
+    #
+    #   when /^(.*)'s profile page$/i
+    #     user_profile_path(User.find_by_login($1))
+
+    else
+      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+        "Now, go and add a mapping in #{__FILE__}"
+    end
   end
 end
+
+World(NavigationHelpers)
--- lib/tasks/cucumber.rake
+++ lib/tasks/cucumber.rake
@@ -1,10 +1,16 @@
+$LOAD_PATH.unshift(RAILS_ROOT + '/vendor/plugins/cucumber/lib') if File.directory?(RAILS_ROOT + '/vendor/plugins/cucumber/lib')
+
 begin
-  $:.unshift(RAILS_ROOT + '/vendor/plugins/cucumber/lib')
   require 'cucumber/rake/task'
 
   Cucumber::Rake::Task.new(:features) do |t|
-    t.cucumber_opts = "--format pretty"
+    t.fork = true
+    t.cucumber_opts = %w{--format pretty}
   end
   task :features => 'db:test:prepare'
 rescue LoadError
+  desc 'Cucumber rake task not available'
+  task :features do
+    abort 'Cucumber rake task is not available. Be sure to install cucumber as a gem or plugin'
+  end
 end
