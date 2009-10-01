0c5b7fbd443105c73aa0eec0e9ffb39d9c472b0b
Patrick Sinclair <metade@livro.local>
2009-10-01 17:05:10 +0100

    Removed unused method

--- app/helpers/bricks_helper.rb
+++ app/helpers/bricks_helper.rb
@@ -1,6 +1,2 @@
 module BricksHelper
-  def brick_image_tag(value)
-    name = (value.blank?) ? 'any' : value.to_i.round
-    image_tag("/images/bricks/_#{name}.jpg", :alt => "£#{name} brick")
-  end
 end
