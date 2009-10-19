63b251e6c084eecadb11ed4666ff054f331cece5
Patrick Sinclair <metade@livro.config>
2009-10-19 22:41:53 +0100

    Refactored swf embed code into a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def embed_wall_swf_tag(brick=nil, thanks=false)
+    brick_link = %[, linkBrick: "#{brick.url_key}", isThanks: #{thanks}] if brick
+    %[<script type="text/javascript">
+    swfobject.embedSWF("http://www.childsifoundation.org/files/buyabrick/wall.swf", "wall", "800", "444", "9.0.0", "", { path: "/wall.xml", config: "http://www.childsifoundation.org/files/buyabrick/xml/config.xml"#{brick_link} }, {allowscriptaccess:"always"});
+    </script>]
+  end
 end
