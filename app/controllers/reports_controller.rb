1c4b868d5159dbf5c36dd01d64565840a4c0253e
Patrick Sinclair <metade@livro.config>
2010-01-12 07:42:11 +0000

    Added a report to list all brick details

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,6 +4,14 @@ class ReportsController < ApplicationController
   def index
   end
   
+  def bricks
+    response.headers['Cache-Control'] = 'public, max-age=30'
+    @bricks = Brick.all(
+      :order => 'purchased_at', 
+      :conditions => ["purchased_at IS NOT NULL"]
+    )
+  end
+  
   def generous_bricks
     @bricks = Brick.paginate(
       :page => params['page'],
