8b6c6ab77b517eddc101c891e5848cc877548828
Patrick Sinclair <metade@gmail.com>
2012-02-19 19:05:59 +0000

    Monkey patching the just giving app until the new version is released

--- config/initializers/justgiving.rb
+++ config/initializers/justgiving.rb
@@ -3,6 +3,20 @@ begin
 rescue NameError => e
 end
 
+module JustGiving
+  class Configuration
+    ## The API endpoint
+    def self.api_endpoint
+      raise JustGiving::InvalidApplicationId.new if !application_id
+      case environment
+        when :sandbox then "https://api-sandbox.justgiving.com/#{application_id}"
+        when :staging then "https://api-staging.justgiving.com/#{application_id}"
+        else "https://api.justgiving.com/#{application_id}"
+      end
+    end
+  end
+end
+
 config_path = File.join(RAILS_ROOT, 'config', 'justgiving.yml')
 if File.exists?(config_path)
   justgiving_options = YAML.load_file(config_path)[RAILS_ENV.to_sym]
