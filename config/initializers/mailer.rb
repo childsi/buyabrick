8af04185533f1b6a01d6d2cb0df5855083f1c5ff
Patrick Sinclair <metade@gmail.com>
2012-02-19 18:54:57 +0000

    Disabling mailer for now

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,15 +1,13 @@
-# require "smtp_tls"
-
-config_path = File.join(RAILS_ROOT, 'config', 'mailer.yml')
-if File.exists?(config_path)
-  config = YAML.load_file(config_path)
-else
-  config = {
-    :address => ENV['MAILER_ADDRESS'] || 'smtp.gmail.com',
-    :port => ENV['MAILER_PORT'] || '587',
-    :user_name => ENV['MAILER_USER_NAME'],
-    :password => ENV['MAILER_PASSWORD'],
-    :authentication => ENV['MAILER_AUTHENTICATION'] || 'plain',
-  }
-end
-ActionMailer::Base.smtp_settings = config
+# config_path = File.join(RAILS_ROOT, 'config', 'mailer.yml')
+# if File.exists?(config_path)
+#   config = YAML.load_file(config_path)
+# else
+#   config = {
+#     :address => ENV['MAILER_ADDRESS'] || 'smtp.gmail.com',
+#     :port => ENV['MAILER_PORT'] || '587',
+#     :user_name => ENV['MAILER_USER_NAME'],
+#     :password => ENV['MAILER_PASSWORD'],
+#     :authentication => ENV['MAILER_AUTHENTICATION'] || 'plain',
+#   }
+# end
+# ActionMailer::Base.smtp_settings = config
