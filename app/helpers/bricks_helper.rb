0e1e58450059fdbe915f9d5c9fc7ca074a7094f6
Patrick Sinclair <metade@gmail.com>
2012-02-19 18:37:47 +0000

    Using the JG environment to configure the JG donation url

--- app/helpers/bricks_helper.rb
+++ app/helpers/bricks_helper.rb
@@ -1,9 +1,9 @@
 module BricksHelper
   def justgiving_link(brick)
-    if Rails.env.production?
-      justgiving_path = "http://www.justgiving.com/donation/direct/charity/185222"
-    else
-      justgiving_path = "http://v3-sandbox.justgiving.com/donation/direct/charity/185222"
+    justgiving_path = case JustGiving::Configuration.environment
+      when :sandbox then "http://v3-sandbox.justgiving.com/donation/direct/charity/185222"
+      when :staging then "http://v3-staging.justgiving.com/donation/direct/charity/185222"
+      else "http://www.justgiving.com/donation/direct/charity/185222"
     end
     amount = brick.value/100.0
     exit_url = return_payment_notification_url(brick) + "?donationId=JUSTGIVING-DONATION-ID"
